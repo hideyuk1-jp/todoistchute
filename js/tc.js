122dba27b5117299670214dd8a811ecd4758cb92
hideyuk1-jp <hh11028+github@gmail.com>
2023-05-03 14:08:06 +0900

    fix: タスクリストを内包するセレクタ修正

--- js/tc.js
+++ js/tc.js
@@ -15,7 +15,7 @@ const defaultTaskBar = "true"; // タスクバー使用の初期値
 
 let tchtml;
 const tcParentId = "#content"; // tcの親要素のID
-const taskListParentId = "#editor"; // タスクリストを内包する要素のID
+const taskListParentId = ".main-view-layout"; // タスクリストを内包する要素のID
 const tcCheckIntervalTime = 300; // タスクリストの変更をチェックする間隔の時間（ミリ秒）
 
 $(async function () {
